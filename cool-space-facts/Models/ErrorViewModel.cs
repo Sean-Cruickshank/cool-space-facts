768b2e7eedb8506da34eb8810a97adce63fb0ac4
Sean Cruickshank <Seancruickshank2019@gmail.com>
2023-12-04 14:41:31 +1300

    Add project files.

---
+++ cool-space-facts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cool_space_facts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
