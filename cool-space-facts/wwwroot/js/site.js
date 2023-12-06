4daed041c93d5ddbf6839fc3b260d09ba4dfd2dd
Sean Cruickshank <Seancruickshank2019@gmail.com>
2023-12-06 20:41:27 +1300

    Got JS for card and sidebar generation working. Some more CSS wizardry

--- cool-space-facts/wwwroot/js/site.js
+++ cool-space-facts/wwwroot/js/site.js
@@ -1,13 +1,25 @@
-﻿const today = dayjs();
-const todayClock = today.format('h:mmA');
-const todayTime = today.format('HH');
+﻿//checks the current time and sets the Earth image accordingly
 const earthImage = document.querySelector('.js-earth-image');
-let numSwitch = 0;
-if (todayTime > 6 && todayTime < 18) {
-    earthImage.innerHTML = '<img id="nav-earth-image" src="/earth-day.png" />';
-} else {
-    earthImage.innerHTML = '<img id="nav-earth-image" src="/earth-night.png" />';
+
+function setEarthImage(today) {
+    const todayTime = today.format('HH');
+    let numSwitch = 0;
+    if (todayTime > 6 && todayTime < 18) {
+        earthImage.innerHTML = '<img id="nav-earth-image" src="/earth-day.png" />';
+    } else {
+        earthImage.innerHTML = '<img id="nav-earth-image" src="/earth-night.png" />';
+    }
 }
 
-const clock = document.querySelector('.js-sidenav-time');
-clock.innerHTML = todayClock;
+
+//gets current time, formats correctly, runs setEarthImage and updates every 15 seconds
+clockRefresh();
+setInterval(clockRefresh, 15000);
+
+function clockRefresh() {
+    const clockSet = dayjs();
+    const clockFormat = clockSet.format('h:mmA');
+    const clock = document.querySelector('.js-sidenav-clock');
+    clock.innerHTML = clockFormat;
+    setEarthImage(clockSet);
+}
---
+++ cool-space-facts/wwwroot/js/size.js
@@ -0,0 +1,41 @@
+﻿const navbarSelect = document.querySelector('.js-nav-link-size');
+navbarSelect.classList.add('nav-link-current');
+
+const sizeList = [
+    {
+        id: 'size-01',
+        title: 'Asteroids',
+        size: '1km',
+        factList: 'There are more asteroids in the solar system, than $100 notes in my wallet'
+    },
+    {
+        id: 'size-02',
+        title: 'Ceres',
+        size: '1000km',
+        factList: 'It is the largest asteroid in the asteroid belt and really should just be called a planet I think'
+    },
+    {
+        id: 'size-03',
+        title: 'The Fucking Moon',
+        size: '2000km',
+        factList: 'Look in the sky!!! You can see it with your eye!'
+    },
+]
+
+let sizeHTML = '';
+let sidebarHTML = '';
+
+sizeList.forEach((item) => {
+    sizeHTML += `
+        <h1 id="${item.id}" class="size-card-title">${item.title}</h1>
+        <p>${item.size}</p>
+        <p>${item.factList}</p>
+        `;
+
+    sidebarHTML += `
+        <a class="nav-side-item" href="#${item.id}">${item.title}</a>
+`
+});
+
+document.querySelector('.js-size-card').innerHTML = sizeHTML;
+document.querySelector('.js-nav-side-content').innerHTML = sidebarHTML;
