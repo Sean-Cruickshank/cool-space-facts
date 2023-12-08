8c30190fe0b2c163f3869b8de713e803ec783957
Sean Cruickshank <Seancruickshank2019@gmail.com>
2023-12-08 15:40:23 +1300

    Added real time highlighting and scrolling for side navbar contents. Added CSS to side navbar.

--- cool-space-facts/wwwroot/js/size.js
+++ cool-space-facts/wwwroot/js/size.js
@@ -3,39 +3,103 @@ navbarSelect.classList.add('nav-link-current');
 
 const sizeList = [
     {
-        id: 'size-01',
+        id: 'size-0',
         title: 'Asteroids',
         size: '1km',
         factList: 'There are more asteroids in the solar system, than $100 notes in my wallet'
     },
     {
-        id: 'size-02',
+        id: 'size-1',
         title: 'Ceres',
         size: '1000km',
         factList: 'It is the largest asteroid in the asteroid belt and really should just be called a planet I think'
     },
     {
-        id: 'size-03',
-        title: 'The Fucking Moon',
+        id: 'size-2',
+        title: 'The Moon',
         size: '2000km',
         factList: 'Look in the sky!!! You can see it with your eye!'
     },
+    {
+        id: 'size-3',
+        title: 'Venus',
+        size: '2000km',
+        factList: 'hot'
+    },
+    {
+        id: 'size-4',
+        title: 'Mercury',
+        size: '2000km',
+        factList: 'less hot somehow'
+    },
+    {
+        id: 'size-5',
+        title: 'The Sun',
+        size: '2000km',
+        factList: 'more hot'
+    }
 ]
 
 let sizeHTML = '';
 let sidebarHTML = '';
 
+
+//Generates HTML cards for each entry in sizeList
 sizeList.forEach((item) => {
     sizeHTML += `
-        <h1 id="${item.id}" class="size-card-title">${item.title}</h1>
-        <p>${item.size}</p>
-        <p>${item.factList}</p>
+        <div class="size-card">
+            <h1 id="${item.id}" class="size-card-title">${item.title}</h1>
+            <p>${item.size}</p>
+          <p>${item.factList}</p>
+        </div>
         `;
 
-    sidebarHTML += `
-        <a class="nav-side-item" href="#${item.id}">${item.title}</a>
-`
+    sidebarHTML += 
+        `
+          <a class="nav-side-item ${item.id}" href="#${item.id}">${item.title}</a>
+        `
+    document.querySelector('.js-size-body').innerHTML = sizeHTML;
+    document.querySelector('.js-nav-side-content').innerHTML = sidebarHTML;
+
 });
 
-document.querySelector('.js-size-card').innerHTML = sizeHTML;
-document.querySelector('.js-nav-side-content').innerHTML = sidebarHTML;
+
+//When the user hovers their mouse over the div for a card, highlight the corresponding entry in the sidenav
+/*
+document.querySelectorAll('.size-card')
+    .forEach((content, index) => {
+        content.addEventListener('wheel', () => {
+            sideNavHighlight(index);
+        });
+    });
+*/
+
+//When the user clicks an entry in the sidenav, highlight that entry
+/*
+document.querySelectorAll('.nav-side-item')
+    .forEach((content, index) => {
+        content.addEventListener('click', () => {
+            sideNavHighlight(index);
+        });
+    });
+*/
+
+window.addEventListener('scroll', () => {
+    const position = Math.floor((scrollY) / 500);
+    console.log(position);
+    console.log(sizeList.length);
+    if (position < sizeList.length) {
+        sideNavHighlight(position);
+    }
+
+});
+
+//Highlights the active card div and removes highlights from all others
+function sideNavHighlight(index) {
+    const test = document.querySelector(`.size-${index}`);
+    const hello = Array.from(document.getElementsByClassName('js-size-card'));
+    hello.forEach((test) => {
+        test.classList.remove('js-size-card');
+    });
+    test.classList.add('js-size-card');
+}
