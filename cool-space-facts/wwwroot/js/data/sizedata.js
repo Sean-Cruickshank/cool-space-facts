a893d087814d054de3dc53093d300b3e46067715
Sean-Cruickshank <seancruickshank2023@gmail.com>
2024-04-01 18:11:53 +1300

    I don't even know anymore

--- cool-space-facts/wwwroot/js/data/sizedata.js
+++ cool-space-facts/wwwroot/js/data/sizedata.js
@@ -2,7 +2,7 @@
     {
         title: 'The International Space Station',
         size: '108m',
-        image: '/images/size/iss.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/iss.jpg',
         imageDesc: 'A view of the International Space Station over Earth',
         factList: [
             'Fact A',
@@ -13,8 +13,9 @@
     {
         title: 'Deimos',
         size: '12.4km',
-        image: '/images/size/deimos.jpg',
-        imageDesc: 'The moon Deimos',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/deimos.jpg',
+        imageDesc: 'The second moon of Mars, Deimos',
+        imageCaption: 'NASA/JPL/Malin Space Science Systems',
         factList: [
             'Fact A',
             'Fact B',
@@ -24,8 +25,9 @@
     {
         title: 'Neutron Stars',
         size: '20km',
-        image: '/images/size/neutron_star.jpg',
-        imageDesc: 'A neutron star',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/neutron_star.jpg',
+        imageDesc: 'An illustration of a neutron star emitting electromagnetic radiation',
+        imageCaption: 'Scott Wiessinger, Francis Reddy, Jeanette Kazmierczak',
         factList: [
             'Fact A',
             'Fact B',
@@ -35,7 +37,7 @@
     {
         title: 'Vesta',
         size: '525km',
-        image: '/images/size/vesta.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/vesta.jpg',
         imageDesc: 'The asteroid Vesta',
         factList: [
             'Fact A',
@@ -46,7 +48,7 @@
     {
         title: 'Ceres',
         size: '946km',
-        image: '/images/size/ceres.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/ceres.jpg',
         imageDesc: 'Dwarf planet and asteroid, Ceres',
         factList: [
             'Fact A',
@@ -57,8 +59,9 @@
     {
         title: 'Pluto',
         size: '2377km',
-        image: '/images/size/pluto.jpg',
-        imageDesc: 'An image of Pluto',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/pluto.jpg',
+        imageDesc: 'An image of Pluto captured by New Horizons',
+        imageCaption: 'NASA',
         factList: [
             'Fact A',
             'Fact B',
@@ -68,7 +71,7 @@
     {
         title: 'The Moon',
         size: '3475km',
-        image: '/images/size/moon.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/moon.jpg',
         imageDesc: 'The Moon in waning gibbous',
         factList: [
             'Fact A',
@@ -79,7 +82,7 @@
     {
         title: 'Mercury',
         size: '4879km',
-        image: '/images/size/mercury.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/mercury.jpg',
         imageDesc: 'An image of Mercury',
         factList: [
             'Fact A',
@@ -90,8 +93,9 @@
     {
         title: 'Ganymede',
         size: '5268km',
-        image: '/images/size/ganymede.jpg',
-        imageDesc: 'Jupiter\'s largest moon, Ganymede',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/ganymede.jpg',
+        imageDesc: 'An image of Jupiter\'s largest moon, Ganymede',
+        imageCaption: 'NASA/JPL',
         factList: [
             'Fact A',
             'Fact B',
@@ -101,7 +105,7 @@
     {
         title: 'Mars',
         size: '6779km',
-        image: '/images/size/mars.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/mars.jpg',
         imageDesc: 'An image of Mars',
         factList: [
             'Fact A',
@@ -112,8 +116,9 @@
     {
         title: 'White Dwarf Stars',
         size: '11,200km - 24,000km',
-        image: '/images/size/white_dwarf_star.jpg',
-        imageDesc: 'A white dwarf star',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/white_dwarf.jpg',
+        imageDesc: 'An illustration of a White Dwarf Star surrounded by a cloud of debris',
+        imageCaption: 'NASA’s Goddard Space Flight Center/Scott Wiessinger',
         factList: [
             'Fact A',
             'Fact B',
@@ -123,7 +128,7 @@
     {
         title: 'Venus',
         size: '12,104km',
-        image: '/images/size/venus.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/venus.jpg',
         imageDesc: 'An image of Venus',
         factList: [
             'Fact A',
@@ -134,7 +139,7 @@
     {
         title: 'Earth',
         size: '12,742km',
-        image: '/images/size/earth.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/earth.jpg',
         imageDesc: 'Cities of Earth at night',
         factList: [
             'Fact A',
@@ -145,7 +150,7 @@
     {
         title: 'Neptune',
         size: '49,244km',
-        image: '/images/size/neptune.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/neptune.jpg',
         imageDesc: 'An image of Neptune',
         factList: [
             'Fact A',
@@ -156,8 +161,9 @@
     {
         title: 'Uranus',
         size: '50,724km',
-        image: '/images/size/uranus.jpg',
-        imageDesc: 'An image of Uranus with rings',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/uranus.jpg',
+        imageDesc: 'An artists depiction of Uranus with rings',
+        imageCaption: 'Planet Volumes',
         factList: [
             'Fact A',
             'Fact B',
@@ -167,8 +173,9 @@
     {
         title: 'Red Dwarf Stars',
         size: '70,000km - 350,000km',
-        image: '/images/size/red_dwarf_star.jpg',
-        imageDesc: 'A red dwarf star',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/red_dwarf.jpg',
+        imageDesc: 'An illustration of a binary Red Dwarf system',
+        imageCaption: 'NASA Goddard',
         factList: [
             'Fact A',
             'Fact B',
@@ -178,8 +185,9 @@
     {
         title: 'Saturn',
         size: '116,460km (282,000km with rings)',
-        image: '/images/size/saturn.jpg',
-        imageDesc: 'An image of Saturn',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/saturn.jpg',
+        imageDesc: 'An image of Saturn with rings',
+        imageCaption: 'NASA/JPL',
         factList: [
             'Fact A',
             'Fact B',
@@ -188,9 +196,10 @@
     },
     {
         title: 'Jupiter',
-        size: '139,820km',
-        image: '/images/size/jupiter.jpg',
-        imageDesc: 'An image of Jupiter',
+        size: '142,984km',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/jupiter.jpg',
+        imageDesc: 'An artists depiction of Jupiter',
+        imageCaption: 'Planet Volumes',
         factList: [
             'Fact A',
             'Fact B',
@@ -200,8 +209,9 @@
     {
         title: 'GQ Lupi b',
         size: '930,000km',
-        image: '/images/size/gq_lupi_b.jpg',
-        imageDesc: 'An image of GQ Lupi B',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/gq_lupi_b.jpg',
+        imageDesc: 'An illustration of a large exoplanet, similar to GQ Lupi B',
+        imageCaption: 'James Cohen',
         factList: [
             'Fact A',
             'Fact B',
@@ -211,7 +221,7 @@
     {
         title: 'The Sun',
         size: '1,392,000km',
-        image: '/images/size/sun.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/sun.jpg',
         imageDesc: 'An image of the Sun',
         factList: [
             'Fact A',
@@ -222,8 +232,9 @@
     {
         title: 'Sagittarius A',
         size: '51,800,000km',
-        image: '/images/size/sagittarius.jpg',
-        imageDesc: 'An image of Sagittarius A',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/sagittarius.jpg',
+        imageDesc: 'An illustration of a supermassive black hole like Sagittarius A',
+        imageCaption: 'NASA/JPL-Caltech',
         factList: [
             'Fact A',
             'Fact B',
@@ -233,8 +244,9 @@
     {
         title: 'UY Scuti',
         size: '2,376,500,000km',
-        image: '/images/size/uy_scuti.jpg',
-        imageDesc: 'An artists depiction of UY Scuti',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/uy_scuti.jpg',
+        imageDesc: 'An image of UY Scuti',
+        imageCaption: 'By ESO/Digitized Sky Survey 2 - DSS2',
         factList: [
             'Fact A',
             'Fact B',
@@ -242,10 +254,11 @@
         ]
     },
     {
-        title: 'TON 618',
-        size: '1,300AU (194.47 Billion km)',
-        image: '/images/size/ton_618.jpg',
-        imageDesc: 'An artists depiction of TON 618',
+        title: 'Phoenix A',
+        size: '590,500,000,000km',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/phoenix_a.jpg',
+        imageDesc: 'An illustration of a supermassive black hole like Phoenix A',
+        imageCaption: 'AlexAntropov86',
         factList: [
             'Fact A',
             'Fact B',
@@ -255,8 +268,9 @@
     {
         title: 'The Oort Cloud',
         size: '800,000AU (119.67 Trillion km)',
-        image: '/images/size/oort_cloud.jpg',
-        imageDesc: 'An illustration of the Oort Cloud',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/oort_cloud.jpg',
+        imageDesc: 'A logarithmic diagram of the Solar System, showing the scale of the Oort Cloud',
+        imageCaption: 'NASA/JPL-Caltech',
         factList: [
             'Fact A',
             'Fact B',
@@ -266,7 +280,7 @@
     {
         title: 'Eagle Nebula',
         size: '70 Light Years (662.25 Trillion km)',
-        image: '/images/size/eagle_nebula.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/eagle_nebula.jpg',
         imageDesc: 'An image of the Eagle Nebula',
         factList: [
             'Fact A',
@@ -277,8 +291,9 @@
     {
         title: 'The Milky Way',
         size: '105,700 Light Years (999.99 Quadrillion km)',
-        image: '/images/size/milky_way.jpg',
-        imageDesc: 'An artists depiction of the Milky Way galaxy',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/milky_way.jpg',
+        imageDesc: 'An image of the Milky Way galaxy as seen from Earth',
+        imageCaption: 'Graham Holtshausen',
         factList: [
             'Fact A',
             'Fact B',
@@ -288,7 +303,7 @@
     {
         title: 'Andromeda',
         size: '152,000 Light Years (1.43 Quintillion km)',
-        image: '/images/size/andromeda.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/andromeda.jpg',
         imageDesc: 'An image of the Andromeda galaxy',
         factList: [
             'Fact A',
@@ -299,7 +314,7 @@
     {
         title: 'ESO 383-G 076',
         size: '1,760,000 Light Years (16.65 Quintillion km)',
-        image: '/images/size/eso_383g_076.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/eso_383g_076.jpg',
         imageDesc: 'An image of ESO 383-G 076',
         factList: [
             'Fact A',
@@ -310,7 +325,7 @@
     {
         title: 'Laniakea Supercluster',
         size: '520,000,000 Light Years',
-        image: '/images/size/laniakea.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/laniakea.jpg',
         imageDesc: 'An illustration of the Laniakea Supercluster',
         factList: [
             'Fact A',
@@ -321,7 +336,7 @@
     {
         title: 'Hercules-Corona Borealis Great Wall',
         size: '10,000,000,000 Light Years',
-        image: '/images/size/hercules_corona.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/hercules_corona.jpg',
         imageDesc: 'An illustration of the Hercules-Corona Borealis Great Wall',
         factList: [
             'Fact A',
@@ -332,7 +347,7 @@
     {
         title: 'The Observable Universe',
         size: '93,016,000,000 Light Years',
-        image: '/images/size/universe.jpg',
+        image: 'https://dqe1enhkuqw3t.cloudfront.net/compare/observable_universe.jpg',
         imageDesc: 'An illustration of the observable universe',
         factList: [
             'Fact A',
